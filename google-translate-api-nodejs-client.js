1d0664ab33f8e7c384687ba4877b6ef16bd9432e
bitliner <giovanni.gaglione@gmail.com>
2015-03-23 21:06:09 +0100

    +test, +conversion of response to normal text

--- google-translate-api-nodejs-client.js
+++ google-translate-api-nodejs-client.js
@@ -1,6 +1,8 @@
 var request = require('request'),
     config = require('./config'),
-    Logger = require('logb').getLogger(module.filename);
+    Logger = require('logb').getLogger(module.filename),
+    Entities = require('html-entities').AllHtmlEntities,
+    entities = new Entities();
 
 var API_KEY = config.GOOGLE_TRANSLATE_API_KEY,
     URL = 'https://www.googleapis.com/language/translate/v2';
@@ -21,18 +23,23 @@ function translate(opts, cb) {
         }
     }, function(err, response, body) {
 
+        var translation;
+
         if (err) {
             cb(err);
             return;
         }
+        body = JSON.parse(body);
+        translation = body.data.translations[0].translatedText;
+        translation = entities.decode(translation);
 
-        return cb(null, body);
+        return cb(null, translation);
 
     });
 };
 
 
-module.export.translate = translate;
+module.exports.translate = translate;
 
 module.exports.fromGermanToEnglish = function(text, cb) {
 
---
+++ test/test.js
@@ -0,0 +1,41 @@
+var expect = require('chai').expect,
+    path = require('path');
+
+var GoogleTranslateApiClient = require('../google-translate-api-nodejs-client');
+
+
+
+
+describe('GOOGLE_TRANSLATE_API_NODEJS_CLIENT', function() {
+
+    var germanText,
+        germanTextEnglishTranslation;
+
+    beforeEach(function() {
+
+        germanText = 'Zur RX100 M2 hatte ich bereits eine sehr ausführliche Rezension geschrieben. Nun habe ich mir die RX100 M3 angeschafft und möchte Besitzern einer RX100 M2 die Unterschiede aufzeigen, damit jeder für sich entscheiden kann, ob sich das Upgrade lohnt oder nicht.';
+        germanTextEnglishTranslation = 'To RX100 M2 I had already written a very detailed review. Now I have bought myself the RX100 RX100 M2 M3 and want a show owners the differences so that everyone can decide for yourself if it\'s worth the upgrade or not.';
+
+
+
+    });
+
+    it('should translate a german text to right english text', function(done) {
+
+
+        GoogleTranslateApiClient.fromGermanToEnglish(germanText, function(err, translation) {
+            expect(err).to.be.null;
+            expect(translation).to.be.not.undefined;
+            expect(translation).to.be.eql(germanTextEnglishTranslation);
+            done();
+        });
+
+
+    });
+
+
+
+
+
+
+})
