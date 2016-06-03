0e28fbb96ff41bd9c52bec85e9e869a950295720
bitliner <giovanni.gaglione@gmail.com>
2016-06-03 12:34:47 +0000

    +detect() method to detect language of a text

--- google-translate-api-nodejs-client.js
+++ google-translate-api-nodejs-client.js
@@ -1,142 +1,221 @@
-/* jshint node:true */
-'use strict';
+ /* jshint node:true */
+ 'use strict';
 
 
-var request = require('request'),
-	Logger = require('logb').getLogger(module.filename),
-	Entities = require('html-entities').AllHtmlEntities,
-	entities = new Entities(),
-	RateLimiter = require('limiter').RateLimiter;
-
-
-
-var GoogleTranslateApi = module.exports = function GoogleTranslateApi(options) {
-	// credentials
-	this.API_KEY = options.API_KEY;
-	this.URL = 'https://www.googleapis.com/language/translate/v2';
-	// throttling
-	this.limiter = new RateLimiter(1, 'second');
-
-
-	Logger.info('Using API_KEY', this.API_KEY);
-};
-GoogleTranslateApi.prototype.translate = function(opts, cb) {
-	var self = this;
-
-	self.limiter.removeTokens(1, function() {
-		self._translate(opts, cb);
-	});
-};
-GoogleTranslateApi.prototype._translate = function translate(opts, cb) {
-	var self = this;
-
-
-	request.post({
-		url: self.URL,
-		headers: {
-			'X-HTTP-Method-Override': 'GET'
-		},
-		form: {
-			key: self.API_KEY,
-			q: opts.text,
-			source: opts.source,
-			target: opts.target
-		}
-	}, function(err, response, body) {
-
-		var translation;
-
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		body = JSON.parse(body);
-
-		if (!body.data || !body.data.translations) {
-			Logger.warn('response from Google Translate API returned weird body', body);
-			return cb(new Error('Weird response from Google Translate API'));
-		}
-
-		translation = body.data.translations[0].translatedText;
-		translation = entities.decode(translation);
-
-		return cb(null, translation);
-
-	});
-};
+ var request = require('request'),
+ 	Logger = require('logb').getLogger(module.filename),
+ 	Entities = require('html-entities').AllHtmlEntities,
+ 	entities = new Entities(),
+ 	RateLimiter = require('limiter').RateLimiter;
 
 
 
-GoogleTranslateApi.prototype.fromGermanToEnglish = function(text, cb) {
+ var GoogleTranslateApi = module.exports = function GoogleTranslateApi(options) {
+ 	// credentials
+ 	this.API_KEY = options.API_KEY;
+ 	this.URL = 'https://www.googleapis.com/language/translate/v2';
+ 	// throttling
+ 	this.limiter = new RateLimiter(1, 'second');
 
-	Logger.info('Translating from German to English...');
 
-	this.translate({
-		source: 'de',
-		target: 'en',
-		text: text
-	}, cb);
+ 	Logger.info('Using API_KEY', this.API_KEY);
+ };
+ GoogleTranslateApi.prototype.translate = function(opts, cb) {
+ 	var self = this;
 
-	return;
-};
-GoogleTranslateApi.prototype.fromDutchToEnglish = function(text, cb) {
+ 	self.limiter.removeTokens(1, function() {
+ 		self._translate(opts, cb);
+ 	});
+ };
+ GoogleTranslateApi.prototype._translate = function translate(opts, cb) {
+ 	var self = this;
 
-	Logger.info('Translating from Dutch to English...');
 
-	this.translate({
-		source: 'nl',
-		target: 'en',
-		text: text
-	}, cb);
+ 	request.post({
+ 		url: self.URL,
+ 		headers: {
+ 			'X-HTTP-Method-Override': 'GET'
+ 		},
+ 		form: {
+ 			key: self.API_KEY,
+ 			q: opts.text,
+ 			source: opts.source,
+ 			target: opts.target
+ 		}
+ 	}, function(err, response, body) {
 
-	return;
-};
-GoogleTranslateApi.prototype.fromSimplifiedChineseToEnglish = function(text, cb) {
+ 		var translation;
 
-	Logger.info('Translating from German to English...');
+ 		if (err) {
+ 			cb(err);
+ 			return;
+ 		}
 
-	this.translate({
-		source: 'zh-CN',
-		target: 'en',
-		text: text
-	}, cb);
+ 		body = JSON.parse(body);
 
-	return;
-};
-GoogleTranslateApi.prototype.fromFrenchToEnglish = function(text, cb) {
+ 		if (!body.data || !body.data.translations) {
+ 			Logger.warn('response from Google Translate API returned weird body', body);
+ 			return cb(new Error('Weird response from Google Translate API'));
+ 		}
 
-	Logger.info('Translating from French to English...');
+ 		translation = body.data.translations[0].translatedText;
+ 		translation = entities.decode(translation);
 
-	this.translate({
-		source: 'fr',
-		target: 'en',
-		text: text
-	}, cb);
+ 		return cb(null, translation);
 
-	return;
-};
-GoogleTranslateApi.prototype.fromSpanishToEnglish = function(text, cb) {
+ 	});
+ };
 
-	Logger.info('Translating from Spanish to English...');
 
-	this.translate({
-		source: 'es',
-		target: 'en',
-		text: text
-	}, cb);
 
-	return;
-};
-GoogleTranslateApi.prototype.fromItalianToEnglish = function(text, cb) {
+ GoogleTranslateApi.prototype.fromGermanToEnglish = function(text, cb) {
 
-	Logger.info('Translating from Italian to English...');
+ 	Logger.info('Translating from German to English...');
 
-	this.translate({
-		source: 'it',
-		target: 'en',
-		text: text
-	}, cb);
+ 	this.translate({
+ 		source: 'de',
+ 		target: 'en',
+ 		text: text
+ 	}, cb);
 
-	return;
-};
+ 	return;
+ };
+ GoogleTranslateApi.prototype.fromDutchToEnglish = function(text, cb) {
+
+ 	Logger.info('Translating from Dutch to English...');
+
+ 	this.translate({
+ 		source: 'nl',
+ 		target: 'en',
+ 		text: text
+ 	}, cb);
+
+ 	return;
+ };
+ GoogleTranslateApi.prototype.fromSimplifiedChineseToEnglish = function(text, cb) {
+
+ 	Logger.info('Translating from German to English...');
+
+ 	this.translate({
+ 		source: 'zh-CN',
+ 		target: 'en',
+ 		text: text
+ 	}, cb);
+
+ 	return;
+ };
+ GoogleTranslateApi.prototype.fromFrenchToEnglish = function(text, cb) {
+
+ 	Logger.info('Translating from French to English...');
+
+ 	this.translate({
+ 		source: 'fr',
+ 		target: 'en',
+ 		text: text
+ 	}, cb);
+
+ 	return;
+ };
+ GoogleTranslateApi.prototype.fromSpanishToEnglish = function(text, cb) {
+
+ 	Logger.info('Translating from Spanish to English...');
+
+ 	this.translate({
+ 		source: 'es',
+ 		target: 'en',
+ 		text: text
+ 	}, cb);
+
+ 	return;
+ };
+ GoogleTranslateApi.prototype.fromItalianToEnglish = function(text, cb) {
+
+ 	Logger.info('Translating from Italian to English...');
+
+ 	this.translate({
+ 		source: 'it',
+ 		target: 'en',
+ 		text: text
+ 	}, cb);
+
+ 	return;
+ };
+
+ GoogleTranslateApi.prototype._detect = function(opts, cb) {
+ 	var text;
+ 	var detectedLanguage;
+
+ 	var self = this;
+
+ 	text = opts.text || null;
+
+
+ 	if (!text) {
+ 		return cb(null);
+ 	}
+
+
+ 	request.post({
+ 		url: self.URL+'/detect',
+ 		headers: {
+ 			'X-HTTP-Method-Override': 'GET'
+ 		},
+ 		form: {
+ 			key: self.API_KEY,
+ 			q: text
+ 		}
+ 	}, function(err, response, body) {
+
+
+ 		if (err) {
+ 			return cb(err);
+ 		}
+
+ 		body = JSON.parse(body);
+
+ 		if (!body.data || !body.data.detections || !body.data.detections.length) {
+ 			Logger.warn('response from Google Translate API returned weird body', body);
+ 			return cb(new Error('Weird response from Google Translate API'));
+ 		}
+
+ 		cb(null, body);
+
+
+
+ 	});
+ };
+
+
+
+ GoogleTranslateApi.prototype.detect = function(opts, cb) {
+
+ 	var detection;
+ 	var confidence;
+ 	var detectedLanguage;
+
+ 	var self = this;
+
+
+ 	self._detect(opts, function(err, body) {
+
+ 		if (err){
+ 			return cb(err);
+ 		}
+ 		detection = body.data.detections[0].sort(function(el1, el2) {
+ 			return el2.confidence - el1.confidence;
+ 		})[0];
+
+ 		detectedLanguage = detection.language;
+ 		confidence = detection.confidence;
+
+ 		if (confidence < 0.9) {
+ 			Logger.warn('Confidence less than 90%', detectedLanguage, text);
+ 		}
+
+
+
+ 		return cb(null, detectedLanguage);
+
+
+ 	});
+ };
--- test/test.js
+++ test/test.js
@@ -6,65 +6,87 @@ var GoogleTranslateApiClient = require('../google-translate-api-nodejs-client');
 
 
 
-
 describe('GOOGLE_TRANSLATE_API_NODEJS_CLIENT', function() {
-
-    var germanText,
-        germanTextEnglishTranslation,
-        chineseText,
-        chineseTextEnglishTranslation,
-        client;
+    var client;
 
     beforeEach(function() {
-
-        germanText = 'Zur RX100 M2 hatte ich bereits eine sehr ausführliche Rezension geschrieben. Nun habe ich mir die RX100 M3 angeschafft und möchte Besitzern einer RX100 M2 die Unterschiede aufzeigen, damit jeder für sich entscheiden kann, ob sich das Upgrade lohnt oder nicht.';
-        germanTextEnglishTranslation = 'To RX100 M2 I had already written a very detailed review. Now I have bought myself the RX100 RX100 M2 M3 and want a show owners the differences so that everyone can decide for yourself if it\'s worth the upgrade or not.';
-
-        chineseText = '1小时充电就好了,试了下,不错,我胡子较多那种,剃后很干净,比我以前的braun5000系声音低不少,震动也低不少,冰感也很爽,1099秒的,值得拥有,只是没有旅行盒...没有旅行盒....';
-        chineseTextEnglishTranslation = '1 hour charge is like, try the next, well, I kind of beard large, very clean after shave, lower than my previous braun5000 system sounds a lot, a lot of vibration is low, the ice being too cool, 1099 seconds and worth having, just do not travel without travel case .... box ...';
-
         client = new GoogleTranslateApiClient({
             API_KEY: process.env.GOOGLE_TRANSLATE_API_KEY
         });
     });
 
-    it('should translate a german text to right english text', function(done) {
+    describe('translate()', function() {
+        var germanText,
+            germanTextEnglishTranslation,
+            chineseText,
+            chineseTextEnglishTranslation;
 
-        if (!process.env.GOOGLE_TRANSLATE_API_KEY) {
-            throw new Error('Please specify the env variable GOOGLE_TRANSLATE_API_KEY');
-        }
+        beforeEach(function() {
 
-        client
-            .fromGermanToEnglish(germanText, function(err, translation) {
-                expect(err).to.be.null;
-                expect(translation).to.be.not.undefined;
-                expect(translation).to.be.eql(germanTextEnglishTranslation);
-                done();
-            });
+            germanText = 'Zur RX100 M2 hatte ich bereits eine sehr ausführliche Rezension geschrieben. Nun habe ich mir die RX100 M3 angeschafft und möchte Besitzern einer RX100 M2 die Unterschiede aufzeigen, damit jeder für sich entscheiden kann, ob sich das Upgrade lohnt oder nicht.';
+            germanTextEnglishTranslation = 'For RX100 M2 I had already written a very detailed review. Now I have bought myself the RX100 M3 and want a RX100 M2 show owners the differences, so that everyone can decide for themselves if it\'s worth the upgrade or not.';
 
+            chineseText = '1小时充电就好了,试了下,不错,我胡子较多那种,剃后很干净,比我以前的braun5000系声音低不少,震动也低不少,冰感也很爽,1099秒的,值得拥有,只是没有旅行盒...没有旅行盒....';
+            chineseTextEnglishTranslation = '1 hour charging enough, the next test, yes, I\'m more the kind of beard, very clean after shaving, lower than my previous braun5000 system sounds a lot, a lot of vibration is low, ice sense of cool, 1099 seconds and worth having, but no travel case ... no travel case ....';
+
+
+        });
+
+        it('should translate a german text to right english text', function(done) {
+
+            if (!process.env.GOOGLE_TRANSLATE_API_KEY) {
+                throw new Error('Please specify the env variable GOOGLE_TRANSLATE_API_KEY');
+            }
+
+            client
+                .fromGermanToEnglish(germanText, function(err, translation) {
+                    expect(err).to.be.null;
+                    expect(translation).to.be.not.undefined;
+                    expect(translation).to.be.eql(germanTextEnglishTranslation);
+                    done();
+                });
+
+
+        });
+
+        it('should translate a chinese text to right english text', function(done) {
+
+            if (!process.env.GOOGLE_TRANSLATE_API_KEY) {
+                throw new Error('Please specify the env variable GOOGLE_TRANSLATE_API_KEY');
+                return;
+            }
+
+            client
+                .fromSimplifiedChineseToEnglish(chineseText, function(err, translation) {
+                    expect(err).to.be.null;
+                    expect(translation).to.be.not.undefined;
+                    expect(translation).to.be.eql(chineseTextEnglishTranslation);
+                    done();
+                });
+
+
+        });
 
     });
 
-    it('should translate a chinese text to right english text', function(done) {
+    describe('detect()', function() {
+        var sentence;
 
-        if (!process.env.GOOGLE_TRANSLATE_API_KEY) {
-            throw new Error('Please specify the env variable GOOGLE_TRANSLATE_API_KEY');
-            return;
-        }
+        beforeEach(function() {
+            sentence = 'Bonjour Ã  tous Une recherche sur le forum n\'a rien donnÃ©.Je suis tombÃ© lÃ  - dessus: http: //www.philips.fr/c-m-pe/oneblade-tonte-et-rasage-visage TestÃ© ici : http://www.lesnumeriques.com/rasoir-electrique/philips-oneblade-p31645/test.html Si d\'aventure l\'un de vous a essayÃ©';
+        });
 
-        client
-            .fromSimplifiedChineseToEnglish(chineseText, function(err, translation) {
-                expect(err).to.be.null;
-                expect(translation).to.be.not.undefined;
-                expect(translation).to.be.eql(chineseTextEnglishTranslation);
+        it('should work fine', function(done) {
+            client.detect({
+                text: sentence
+            }, function(err, detectedLanguage) {
+                expect(err).to.be.eql(null);
+                expect(detectedLanguage).to.be.eql('fr');
                 done();
-            });
-
+            })
+        });
 
     });
-
-
-
 
 
 
